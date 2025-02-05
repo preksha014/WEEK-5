3cc94f3c3f40aeb15fefc13b96cf40560bc0516b
Preksha <ranapreksha8@gmail.com>
2025-02-05 17:20:33 +0530

    Added Changes

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//echo
 echo "hello world";
 echo 10 + 90;
 echo "<br/>";
@@ -13,16 +14,13 @@ $a = $b = 90;
 echo $a + $b;
 
 //php with html,css
-echo "<h1 style='color:blue'>Hello world</h1>"
-
-    ?>
+echo "<h1 style='color:blue'>Hello world</h1>" ?>
 
 <h1>Hello my name is
     <?php
     echo $name;
     ?>
 </h1>
-
 
 <?php
 $h3_color = "red";
@@ -42,7 +40,6 @@ echo var_dump(data);
 //operators
 $a = 34;
 $b = 10;
-
 echo $a % $b;
 echo '<br/>';
 echo $a ** $b;
@@ -50,7 +47,6 @@ echo '<br/>';
 
 $a = 10;
 $b = 90;
-
 echo var_dump($a <=> $b);
 echo '<br/>';
 echo $a++;
@@ -83,36 +79,40 @@ echo '<br/>';
 //functions
 //nested function
 $age;
-function test($age){
+function test($age)
+{
     echo "age is less<br/>";
-    if($age>18){
-        function display($age){
+    if ($age > 18) {
+        function display($age)
+        {
             echo "age is $age<br/>";
         }
-       display($age);
+        display($age);
     }
 }
 test(78);
 
 //variable function
-
-function child(){
+function child()
+{
     echo "child function is called<br/>";
 }
 
-$a="child";
+$a = "child";
 $a();
 
-function parent($x){
+function parent($x)
+{
     echo "parent function is called<br/>";
     $x();
 }
 parent($a);
 
+
 //In-built functions
-$str="Preksha rana here";
+$str = "Preksha rana here";
 echo strlen($str);
-echo substr($str,8);
+echo substr($str, 8);
 echo "<br/>";
 
 echo date("d");
@@ -123,85 +123,90 @@ echo "<br/>";
 echo is_string($str);
 echo "<br/>";
 
-$num=0;
+$num = 0;
 echo is_int($num);
 echo "<br/>";
 
-echo rand();echo "<br/>";
+echo rand();
+echo "<br/>";
 
 // die;
 // exit();
 
 //Arrays-->indexed array, associative array,multi-dimensional array
-$products=["laptop","television","mobile"];
+$products = ["laptop", "television", "mobile"];
 
-foreach($products as $i){
+foreach ($products as $i) {
     echo $i;
     echo "<br>";
 }
 
-$productDetails=[
-    "name"=>"laptop",
-    "price"=>"200000",
-    "quality"=>"good"
+$productDetails = [
+    "name" => "laptop",
+    "price" => "200000",
+    "quality" => "good"
 ];
 
-foreach($productDetails as $key=> $i){
-    echo $key ." is ".$i;
+foreach ($productDetails as $key => $i) {
+    echo $key . " is " . $i;
     echo "<br>";
 }
 
-$productss=[
-    [1,"laptop","200000","good"],
-    [2,"television","80000","avearge"]
+$productss = [
+    [1, "laptop", "200000", "good"],
+    [2, "television", "80000", "avearge"]
 ];
 
 // echo "<pre>";
 // print_r($productss);
 // echo "</pre>";
 
-for($i=0;$i<count($productss);$i++){
-    for($j=0;$j<count($productss[$i]);$j++){
+for ($i = 0; $i < count($productss); $i++) {
+    for ($j = 0; $j < count($productss[$i]); $j++) {
         echo $productss[$i][$j];
         echo "<br>";
     }
-} 
+}
 
 //Add and Remove array elements
-array_push($products,"washing machine");
-print_r($products);echo "<br/>";
+array_push($products, "washing machine");
+print_r($products);
+echo "<br/>";
 array_pop($products);
-print_r($products);echo "<br/>";
-array_splice($products,-1);
+print_r($products);
+echo "<br/>";
+array_splice($products, -1);
 print_r($products);
 
 //Array functions
-$users=["damon","stefan","elena"]; echo "<br/>";
+$users = ["damon", "stefan", "elena"];
+echo "<br/>";
 // $users=10;
 // print_r($users); echo "<br/>";
-if(is_array($users)){
-    foreach($users as $x){
+if (is_array($users)) {
+    foreach ($users as $x) {
         echo $x;
         echo "<br>";
     }
-}else{
+} else {
     echo "Not an array";
 }
 
 // unset($users[1]);
 // print_r($users);
 
-array_push( $users,"klaus");
+array_push($users, "klaus");
 print_r($users);
 echo "<br>";
 print_r(array_keys($productDetails));
 
 
 //global and local variables
-$number=10;
-function getNumber(){
+$number = 10;
+function getNumber()
+{
     global $number;
-    $number=90;
+    $number = 90;
     echo $number;
 }
 echo "<br/>";
@@ -216,5 +221,123 @@ echo $number;
 // echo "<pre>";
 // print_r($_SERVER);
 // echo "</pre>";
+?>
 
+<!-- Cookies -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="text" name="username" placeholder="Enter your username">
+        <br>
+        <br>
+        <button name="button" value="set">Set Cookies</button>
+        <br>
+        <br>
+        <button name="button" value="display">Display Cookies</button>
+        <br>
+        <br>
+        <button name="button" value="delete">Delete Cookies</button>
+        <br>
+        <br>
+        <button name="button" value="setSession">Set Session</button>
+        <br>
+        <br>
+        <button name="button" value="getSession">Get Session</button>
+        <br>
+        <br>
+        <button name="button" value="deleteSession">Delete Session</button><br><br>
+        <input type="text" name="filename" placeholder="Enter file name"><br><br>
+        <textarea name="content" placeholder="Enter content of file"></textarea><br><br>
+        <button>Create File</button><br><br>
+        <input type="file" name="readfile"><br><br>
+        <button>Read File</button>
+    </form>
+</body>
+
+</html>
+
+<?php
+//Cookies
+//setcookie(name,value,expires,path,domain,secure, httponly)
+// echo "<br/>";
+// setcookie("fruit", "apple", time() + 86400);
+// if (isset($_COOKIE['fruit'])) {
+//     echo $_COOKIE['fruit'];
+//     echo "<br/>";
+//     echo "<br/>";
+//     echo "<br/>";
+// } else {
+//     echo "cookie is not set";
+// }
+
+// if (isset($_POST['button'])) {
+//     if ($_POST['button'] == "set") {
+//         $val = $_POST['username'];
+//         //echo $val;
+//         setcookie("username", $val);
+//         echo "cookie is set";
+//     }
+//     if ($_POST['button'] == "display") {
+//         if (isset($_COOKIE['username'])) {
+//             echo $_COOKIE['username'];
+//         }
+//     }
+//     if ($_POST['button'] == "delete") {
+//         if (isset($_COOKIE['username'])) {
+//             setcookie("username", null, -1);
+//         }
+//     }
+// }
+
+//Session
+// session_start();
+
+// $_SESSION["name"] = "preksha";
+// echo "<br/>";
+// $_SESSION["email"] = "preksha@gmail.com";
+// echo "<br/>";
+// session_unset();
+// session_destroy();
+// print_r($_SESSION);
+
+// if(isset($_POST["button"])){
+//     if($_POST['button']=="setSession"){
+//         $val = $_POST["username"];
+//         $_SESSION["username"]=$val;
+//     }
+
+// }
+
+//File operations
+// if (isset($_POST['filename'])) {
+//     $fileName = "files/" . $_POST['filename'];
+//     $content = $_POST['content'];
+//     $file = fopen($fileName, "w") or die("unable to create file");
+//     fwrite($file, $content);
+//     fclose($file);
+//     echo "file is created";
+// }
+
+// if(isset($_FILES['readfile'])) {
+//     $filename=$_FILES['readfile']['tmp_name'];
+//     $myFile=fopen($filename,"r");
+//     echo fread($myFile,filesize($filename)) or die("file name is not valid");
+//     fclose($myFile);
+// }
+
+$path = "files";
+$items = scandir($path);
+$items = array_diff($items, array(".", ".."));
+foreach ($items as $item) {
+    echo "<a href=./files/$item>$item</a>";
+    echo "<br/>";
+}
 ?>
--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-
     // if($_GET){
     //     echo "User name is ".$_GET['username'];
     //     echo "User password is ".$_GET['password'];
@@ -10,8 +9,17 @@
     //     echo "User password is ".$_POST['password'];
     // }
 
-    if($_REQUEST){
-        echo 'User name is '.$_REQUEST['username'];echo "<br/>";
-        echo 'User password is '.$_REQUEST['password'];
+    // if($_REQUEST){
+    //     echo 'User name is '.$_REQUEST['username'];echo "<br/>";
+    //     echo 'User password is '.$_REQUEST['password'];
+    // }
+
+    print_r($_FILES);
+
+    if($_FILES['fileUpload']){
+        $path='./uploads/'.$_FILES['fileUpload']['name'];
+        if(move_uploaded_file($_FILES['fileUpload']['tmp_name'],$path)){
+            echo "file uploaded";
+        } else{ echo "file is not uploaded";}
     }
 ?>
